b830931cf855298b80b6168907f2a230a4e0a6f5
Yama machi <s2223199@cco.kanagawa-it.ac.jp>
2025-10-16 14:22:09 +0900

    研究用コンピュータ向けに修正

--- dougaka.py
+++ dougaka.py
@@ -1,11 +1,11 @@
 import yt_dlp
 
-url='https://youtu.be/6sDgjJSoAyY'
+url='https://youtu.be/6sDgjJSoAyY' #変換したい動画のリンク
 
 ydl_opts={
-    'format':'bestvideo+bestaudio/best',
+    'format':'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best',
     'merge_output_format':'mp4',
-    'outtmpl':r'C:\Users\moroz\Videos\mp4\%(title)s.%(ext)s',
+    'outtmpl':r'C:\Users\admin\Videos\movie\%(title)s.%(ext)s', #コンピュータの保存先を指定
     'ffmpeg_location':'ffmpeg.exe',
     'continuedl':True
 }
